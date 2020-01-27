98ed7dec2b7c29a7fb6f7238af800d1cbed2709e
unknown <spdlqj011@gmail.com>
2020-01-27 18:44:03 +0900

    Calling movie list api

--- src/App.js
+++ src/App.js
@@ -1,61 +1,34 @@
 import React from 'react';
-import propTypes from "prop-types";
+import axios from 'axios';
 
-const foodILike = [
-  {
-    id: 1,
-    name: "aaaa",
-    image: "http://recipe1.ezmember.co.kr/cache/recipe/2015/09/18/2eb48c6c35a13139ee738d01016eadf61.jpg",
-    rating: 4
-  },
-  {
-    id: 2,
-    name: "bbbb",
-    image: "2222",
-    rating: 3.5
-  },
-  {
-    id: 3,
-    name: "cccc",
-    image: "3333",
-    rating: 2.8
-  },
-  {
-    id: 4,
-    name: "dddd",
-    image: "4444",
-    rating: 5
-  }
-];
+class App extends React.Component {
 
-function Food({name, picture, rating}) {
-  return <div>
-    <h2>You like {name}?</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} />
-  </div>;
-};
+  state = {
+    isLoading: true
+  };
 
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired,
-  rating: propTypes.number.isRequired
-};
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+  };
+  // JavaScript 코드
+  // async 를 통해 componentDidMount 가 실행 되기 전까지
+  // 잠시 기다림을 알리는데(하나의 액션 ex. Loading..) 어떤 데이터를 기다리냐면
+  // await 을 통해 표시한 url 의 데이터임을 표시해줌.
+  // async 를 먼저 사용함을 알려줘야 await 사용가능.
 
-function App() {
-  return (
-    <div>
-      <h1>Hello!!!!</h1>
-      {
-        foodILike.map(dish => (
-          <Food 
-          key={dish.id} 
-          name={dish.name} 
-          picture={dish.image} 
-          rating={dish.rating} />
-        ))}
+  componentDidMount() {
+    this.getMovies()
+  };
+
+  render() {
+    const { isLoading } = this.state;
+
+    return <div>
+      {/* 3항 연산자 JavaScript 사용 */}
+      {this.state.isLoading ? "Loading.." : "We are ready" }
     </div>
-  );
-};
+  };
+
+}
 
 export default App;

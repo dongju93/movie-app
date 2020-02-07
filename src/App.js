bb04245ba18e12b04e62575820dc778935fa1154
unknown <spdlqj011@gmail.com>
2020-02-07 14:51:02 +0900

    simple navigation with linked pages

--- src/App.js
+++ src/App.js
@@ -1,64 +1,18 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './app.css';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Detail from './routes/Detail';
+import Navigation from './components/Navigation';
+import './app.css'
 
-class App extends React.Component {
-
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  getMovies = async () => {
-    const { 
-      data: { 
-        data: { 
-          movies
-         } 
-      } 
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false });
-  };
-  // JavaScript 코드
-  // async 를 통해 componentDidMount 가 실행 되기 전까지
-  // 잠시 기다림을 알리는데(하나의 액션 ex. Loading..) 어떤 데이터를 기다리냐면
-  // await 을 통해 표시한 url 의 데이터임을 표시해줌.
-  // async 를 먼저 사용함을 알려줘야 await 사용가능.
-
-  componentDidMount() {
-    this.getMovies()
-  };
-
-  render() {
-    const { isLoading, movies } = this.state;
-
-    return (
-      <section className = "container">
-        {/* 3항 연산자 JavaScript 사용 */}
-        {isLoading ? (
-          <div className = "loader">
-            <span className = "loader_text">Loading..</span>
-          </div>
-        ) : (
-          <div className = "movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    )
-  };
-
+function App() {
+  return <HashRouter>
+    <Navigation></Navigation>
+    <Route path='/' exact={true} component={Home} />
+    <Route path='/about' component={About} />
+    <Route path='/movie/:id' component={Detail} />
+  </HashRouter>
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './about.css'
+
+function About(props) {
+    console.log(props)
+    return<span>About page</span>
+}
+
+export default About;
